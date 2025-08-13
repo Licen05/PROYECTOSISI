a0bdf3e881600060b1f16895fb9696f4e5223f5b
Licen05 <afterthecat21@gmail.com>
2025-08-13 15:42:53 -0400

    sss

--- FormEdit.php
+++ FormEdit.php
@@ -7,8 +7,11 @@
         <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
 
-  <link href="CSS/form_regis.css" rel="stylesheet" type="text/css" />
+  <link href="CSS/edit.css" rel="stylesheet" type="text/css" />
     <title>Document</title>
+    <style>
+
+    </style>
 </head>
 <body>
     <?php
