617b3e7227313bf9c6040df35b58180e208e5038
Hec-01 <hectornicolascolquevargas@gmail.com>
2025-08-13 15:28:12 -0400

    asies

--- FormEdit.php
+++ FormEdit.php
@@ -47,30 +47,59 @@ if ($resultado->num_rows > 0) {
 }
 
     ?>
+  <script src="script.js"></script> 
+ 
+  <header class="hea">
+       <?php include("encabezado.php"); ?>
+  </header>
 
+  <div class="cuerpo">
+  
+  <?php include("barra_iz.php"); ?>
 
-    <form action="RegistroEdit.php" method="post">
-    <label for="">Nombres</label> <br>
-    <input type="text" name="nom" value='<?=$Nombres?>'><br>
-    <label for="">Apellidos</label> <br>
-    <input type="text" name="ape" value='<?=$Apellidos?>'><br>
-    <label for="">Contraseña</label> <br>
-    <input type="password" name="contra" value='<?=$Contrasena?>'><br>
-    <label for="">Fecha de Nacimiento</label><br>
-    <input type="date" name="fecha" value='<?=$FechaNacimiento?>'><br>
-        <label for="">Curso</label><br>
-    <input type="text" name="curso" value='<?=$Curso?>'><br>
+  <section class="centro">
 
-    <label for="">Telefono</label><br>
-    <input type="text" name="telef" value='<?=$Telefono?>'><br>
-    <label for="">Direccion</label><br>
-    <input type="text" name="dire" value='<?=$Direccion?>'><br>
+            <div class="espacios">
+
+    <form action="RegistroEdit.php" method="post" id="form_registro"><h1 id="t_registro">EDITA TUS DATOS</h1>
+                    <div id="label">
+    <label for="" class="label_registro" >Nombres</label> <br>
+    <input type="text" name="nom" value='<?=$Nombres?>'  class="registro_espacios"><br>
+    <label for="" class="label_registro" >Apellidos</label> <br>
+    <input type="text" name="ape" value='<?=$Apellidos?>'  class="registro_espacios"><br>
+    <label for="" class="label_registro" >Contraseña</label> <br>
+    <input type="password" name="contra" value='<?=$Contrasena?>'  class="registro_espacios"><br>
+    <label for="" class="label_registro" >Fecha de Nacimiento</label><br>
+    <input type="date" name="fecha" value='<?=$FechaNacimiento?>'  class="registro_espacios"><br>
+           <div class="row"> 
+    <label for="" class="label_registro" >Curso</label><br>
+    <input type="text" name="curso" value='<?=$Curso?>' class="registro_espacios"><br>
+    </div>
+    <label for="" class="label_registro" >Telefono</label><br>
+    <input type="text" name="telef" value='<?=$Telefono?>' class="registro_espacios"><br>
+    <label for="" class="label_registro" >Direccion</label><br>
+    <input type="text" name="dire" value='<?=$Direccion?>' class="registro_espacios"><br>
   
     
     <input type="hidden" name="CI" value='<?=$CI?>'><br>
-    <input type="submit" name="enviar">
+    
+         <div id="botones">  
+    <input type="submit" name="enviar" id="form_enviar" class="form_botones">
 
+             </div>  
+
+         </div>
+    </div> 
+
+  </section>
     </form>
+  <section class="b_derecha"> </section>
+</form>
+  </div>
+   <?php
+    include("footer.php");
+    ?>
+
 
     <script>
         $("form").validate({
