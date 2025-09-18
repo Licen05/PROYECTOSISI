a77880d40d306a885ef70865a7f867d44c68de3a
Licen05 <afterthecat21@gmail.com>
2025-09-18 11:22:32 -0400

    Lu
    letras y posicion de calendario

--- calendario.php
+++ calendario.php
@@ -54,9 +54,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['comen'])) {
     
     body{
       margin: 0px;
+      font-family:'Graduate','serif';
     }
     .bienvenida{
       display: flex;
+      flex-direction:column;
       text-align: center;
       width: 80%;
       padding: 10px;
